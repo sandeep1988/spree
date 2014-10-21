880e2c6b8f59abfb221e76e3f287fd0aced04734
NTF-39 <ntf-39@NTF-39.(none)>
2014-10-21 13:14:02 +0530

    css fine

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,11 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.initialize_on_precompile = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
